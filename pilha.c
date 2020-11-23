b09790123867b95d670c3c4f62f8c0f8bf6e0c1a
Tales Ferreira <talesmateus1999@hotmail.com>
2020-11-23 09:17:38 -0400

    Implemented push function

--- pilha.c
+++ pilha.c
@@ -11,11 +11,14 @@ typedef struct t {
 } stack;
 
 stack * create();
+void push(stack *p, int i);
 
 int main() {
 	stack *p;
 	p=create();
 	printf("%p\n", p->top);
+	push(p, 1);
+	printf("%d\n", p->top->num);
 	return 0;
 }
 
@@ -23,4 +26,12 @@ stack * create() {
 	stack *p = malloc(sizeof(stack));
 	p->top=NULL;
 	return p;
+}
+
+void push(stack *p, int i) {
+	no *aux;
+	aux = malloc(sizeof(no));
+	aux->num=i;
+	aux->prox=p->top;
+	p->top=aux;
 }
