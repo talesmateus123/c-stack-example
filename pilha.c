176e4d0dc661b68ebca37a96713ad09dcbddb8c1
Tales Ferreira <talesmateus1999@hotmail.com>
2020-11-23 09:22:48 -0400

    Implemented pop function

--- pilha.c
+++ pilha.c
@@ -12,12 +12,18 @@ typedef struct t {
 
 stack * create();
 void push(stack *p, int i);
+int pop(stack *p);
 
 int main() {
 	stack *p;
 	p=create();
 	printf("%p\n", p->top);
 	push(p, 1);
+	push(p, 2);
+	push(p, 3);
+	printf("%d\n", p->top->num);
+	printf("%d\n", pop(p));
+	printf("%d\n", pop(p));
 	printf("%d\n", p->top->num);
 	return 0;
 }
@@ -34,4 +40,15 @@ void push(stack *p, int i) {
 	aux->num=i;
 	aux->prox=p->top;
 	p->top=aux;
+}
+
+int pop(stack *p) {
+	no *aux;
+	int num;
+	aux = p->top;
+	p->top=p->top->prox;
+	aux->prox=NULL;
+	num=aux->num;
+	free(aux);
+	return num;
 }
